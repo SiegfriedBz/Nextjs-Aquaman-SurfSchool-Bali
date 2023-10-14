c7a5e1a730a68bd98cbfd0a84f0171b5b75a1666
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-14 21:28:28 +0200

    add Carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,49 @@
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
+import styles from './carousel.module.css'
+import Image from 'next/image'
+
+const Carousel = ({ children }) => {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
+
+  return (
+    <div className={styles.embla} ref={emblaRef}>
+      <div className={styles.embla__container}>{children}</div>
+    </div>
+  )
+}
+export default Carousel
+
+export const CustomCarousel = ({
+  images,
+  carouselId = '',
+  carouselKey = '',
+  carouselClasses = '',
+  imageClasses = '',
+  ...rest
+}) => {
+  return (
+    <Carousel>
+      {images.map((image) => {
+        return (
+          <div
+            key={`${carouselId}-${carouselKey}-${image.src}`}
+            className={`${styles.embla__slide} ${carouselClasses}`}
+          >
+            <Image
+              width='600'
+              height='600'
+              src={image.src}
+              alt={image.alt}
+              placeholder='blur'
+              blurDataURL={image.blurDataUrl}
+              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+              className={imageClasses}
+              {...rest}
+            />
+          </div>
+        )
+      })}
+    </Carousel>
+  )
+}
