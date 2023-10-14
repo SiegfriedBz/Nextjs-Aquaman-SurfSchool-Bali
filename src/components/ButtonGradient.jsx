0263a6e5a689bcbf0825252c086f5f7e94fd69dc
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-14 21:29:58 +0200

    add LinkGradient variants

--- src/components/ButtonGradient.jsx
+++ src/components/ButtonGradient.jsx
@@ -1,14 +1,37 @@
-const ButtonGradient = ({
-  btnVariant = 'btn-gradient',
+import Link from 'next/link'
+
+export const ButtonGradient = ({
+  variant = 'btn-gradient',
   padding = 'px-8 py-4',
   extraClasses = '',
+  onClick = () => {},
   children,
 }) => {
   return (
-    <button className={`btn ${btnVariant} ${padding} ${extraClasses}`}>
+    <button
+      onClick={onClick}
+      className={`btn ${variant} ${padding} ${extraClasses}`}
+    >
       {children}
     </button>
   )
 }
 
-export default ButtonGradient
+export const LinkGradient = ({
+  variant = 'btn-gradient-link',
+  padding = 'px-8 py-4',
+  extraClasses = '',
+  href = process.env.NEXT_PUBLIC_WHATSAPP_LINK || '/',
+  target = '_blank',
+  children,
+}) => {
+  return (
+    <Link
+      href={href}
+      target={target}
+      className={`base-link btn-link ${variant} ${padding} ${extraClasses}`}
+    >
+      {children}
+    </Link>
+  )
+}
