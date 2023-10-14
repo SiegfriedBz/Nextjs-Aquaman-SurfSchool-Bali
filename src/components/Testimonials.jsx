8d6c0ccff963c4e42bbf829b8c6f2f0b7904efba
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-14 21:30:34 +0200

    add Testimonials

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,86 @@
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import Carousel from './carousel'
+import styles from './carousel.module.css'
+import googleIcon from '../../public/logos/google_g_icon.png'
+import { TESTIMONIALS_QUOTES } from '../data/testimonials'
+
+const renderStars = Array.from({ length: 5 }).map((_, i) => {
+  return <FontAwesomeIcon key={`star-${i}`} icon={faStar} />
+})
+
+export const Testimonials = () => {
+  return (
+    <div className='flex flex-col'>
+      <section id='carousel-reviews'>
+        <Carousel>
+          {TESTIMONIALS_QUOTES.map((quote, i) => {
+            return (
+              <div key={`quote-${i}`} className={styles.embla__slide}>
+                <figure className='mb-4'>
+                  <blockquote className='relative rounded-3xl py-6 pl-12 pr-8 '>
+                    <p
+                      className='mx-2 mt-2 line-clamp-4
+                        px-2 text-xl text-slate-900
+                        before:absolute before:left-0 before:top-0
+                        before:translate-x-2 before:translate-y-2
+                        before:transform before:font-serif before:text-9xl
+                      before:text-slate-600 before:opacity-25 before:content-["\201C"]
+                        after:absolute after:-bottom-20 after:right-0 after:-translate-x-2
+                        after:-translate-y-2 after:transform after:font-serif after:text-9xl
+                      after:text-slate-600 after:opacity-25 after:content-["\201D"]
+                      dark:text-slate-400 md:text-2xl'
+                    >
+                      {quote.message}{' '}
+                    </p>
+                    <a
+                      href='https://goo.gl/maps/iHPokm4Q943N2f2fA'
+                      target='_blank'
+                      className='ms-4 mt-2 flex items-center
+                        text-xl italic text-slate-500 outline-none
+                        md:mt-8 md:text-xl'
+                    >
+                      Read full review on{' '}
+                      <Image
+                        src={googleIcon}
+                        width={40}
+                        height={40}
+                        alt='google-icon'
+                        className='ml-2 md:h-16 md:w-16'
+                      />
+                    </a>
+                  </blockquote>
+                  <figcaption className='mt-2 text-right text-xl italic text-slate-500 dark:text-slate-400 md:text-2xl'>
+                    &#8212; {quote.author}
+                  </figcaption>
+                </figure>
+              </div>
+            )
+          })}
+        </Carousel>
+      </section>
+    </div>
+  )
+}
+
+export const TestimonialsLinks = () => {
+  return (
+    <section id='all-reviews-link' className='mx-auto mb-3 mt-2'>
+      <a
+        href='https://goo.gl/maps/iHPokm4Q943N2f2fA'
+        target='_blank'
+        className=''
+      >
+        <h3 className='text-center text-xl md:text-4xl'>
+          Top-rated Surf School
+        </h3>
+        <div className='mt-2 flex items-center'>
+          <span className='m-auto text-4xl font-bold text-amber-400 hover:text-amber-500 md:text-5xl'>
+            {renderStars}
+          </span>
+        </div>
+      </a>
+    </section>
+  )
+}
---
+++ src/data/testimonials.js
@@ -0,0 +1,38 @@
+export const TESTIMONIALS_QUOTES = [
+  {
+    id: 1,
+    message:
+      'Amazing Teacher! It was my first time on a surfboard and due to his easily understandable description of the movements and his broad knowledge in surfing, I managed to stand up quickly',
+    author: 'Laura Kiefer',
+  },
+  {
+    id: 2,
+    message:
+      ' Rendy is the greatest surf instructor I ever had in all my years of surfing 😊 He is always with you in the water, pushing you to catch waves, taxiing you to the peak when your shoulders are soar, keeping you safe when a big set is coming. Moreover, Rendy helped make huge progress thanks to his tips: each new wave was an opportunity to feel the effectiveness of Rendy’s advices 😊',
+    author: 'Francois Gauthier',
+  },
+  {
+    id: 3,
+    message:
+      'I had an absolutely amazing experience learning to surf with Rendy! As a complete newbie with zero experience, I was a bit nervous, but he made it super easy and fun! He broke down the techniques in a way that was easy to understand and provided personalized guidance to help me catch my first waves. Not only a great instructor but also an incredibly friendly and patient person!',
+    author: 'Katrina Ramos',
+  },
+  {
+    id: 4,
+    message:
+      'Rendy - aka Aquaman, is definitely the BEST Surf teacher I ever had - from far! I took surf lessons with plenty of different teachers and I can say that a good teacher such as Rendy is rare and really make a difference in the surfing experience!',
+    author: 'Emilie Lureau',
+  },
+  {
+    id: 5,
+    message:
+      "I had an amazing experience learning to surf with Rendy! You can really feel his passion for the sport. His patient and encouraging teaching style made it easy for a beginner like me to catch some waves. I felt safe and confident in his guidance throughout the lesson. I can't wait to ride more waves with him in the future! Highly recommended. 🌊",
+    author: 'Nana',
+  },
+  {
+    id: 6,
+    message:
+      'Rendy is an amazing surf instructor. I had a fantastic experience with him. He takes time to explain the basics and make sure you understand and apply his knowledge.  And he is awesomely skilled, I was very impressed. Plus, he is super super nice ) ',
+    author: 'Marcello',
+  },
+]
