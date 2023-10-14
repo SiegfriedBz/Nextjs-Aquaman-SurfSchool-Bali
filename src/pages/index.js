ea9e095b9e931678ebd382ca85e50b90c3c7886c
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-14 17:20:21 +0200

    add sub-hero enter animation to prevent layout flash

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,56 +24,61 @@ export default function Home() {
       <section id='hero'>
         <Hero ref={heroRef} />
       </section>
+      <div id='sub-hero'>
+        <section id='school-carousel'>
+          school-carouselIm baby copper mug PBR&B craft beer lo-fi cornhole pork
+          belly vaporware blog hot chicken lyft tattooed. Hammock bruh tote bag,
+          cupping fingerstache flannel affogato enamel pin echo park pabst
+          typewriter gochujang blog green juice. Man braid flexitarian tofu,
+          seitan mixtape crucifix pabst bitters gorpcore skateboard craft beer.
+          Mukbang flexitarian salvia, microdosing copper mug vinyl Brooklyn man
+          braid kombucha mlkshk blog scenester adaptogen snackwave. Ennui
+          cupping hot chicken cliche DSA. Tote bag celiac 90s, fanny pack banh
+          mi migas
+        </section>
 
-      <section id='school-carousel'>
-        school-carouselIm baby copper mug PBR&B craft beer lo-fi cornhole pork
-        belly vaporware blog hot chicken lyft tattooed. Hammock bruh tote bag,
-        cupping fingerstache flannel affogato enamel pin echo park pabst
-        typewriter gochujang blog green juice. Man braid flexitarian tofu,
-        seitan mixtape crucifix pabst bitters gorpcore skateboard craft beer.
-        Mukbang flexitarian salvia, microdosing copper mug vinyl Brooklyn man
-        braid kombucha mlkshk blog scenester adaptogen snackwave. Ennui cupping
-        hot chicken cliche DSA. Tote bag celiac 90s, fanny pack banh mi migas
-      </section>
+        <hr />
 
-      <hr />
+        <section id='about-me'>
+          about-me Im baby copper mug PBR&B craft beer lo-fi cornhole pork belly
+          vaporware blog hot chicken lyft tattooed. Hammock bruh tote bag,
+          cupping fingerstache flannel affogato enamel pin echo park pabst
+          typewriter gochujang blog green juice. Man braid flexitarian tofu,
+          seitan mixtape crucifix pabst bitters gorpcore skateboard craft beer.
+          Mukbang flexitarian salvia, microdosing copper mug vinyl Brooklyn man
+          braid kombucha mlkshk blog scenester adaptogen snackwave. Ennui
+          cupping hot chicken cliche DSA. Tote bag celiac 90s, fanny pack banh
+          mi migas
+        </section>
 
-      <section id='about-me'>
-        about-me Im baby copper mug PBR&B craft beer lo-fi cornhole pork belly
-        vaporware blog hot chicken lyft tattooed. Hammock bruh tote bag, cupping
-        fingerstache flannel affogato enamel pin echo park pabst typewriter
-        gochujang blog green juice. Man braid flexitarian tofu, seitan mixtape
-        crucifix pabst bitters gorpcore skateboard craft beer. Mukbang
-        flexitarian salvia, microdosing copper mug vinyl Brooklyn man braid
-        kombucha mlkshk blog scenester adaptogen snackwave. Ennui cupping hot
-        chicken cliche DSA. Tote bag celiac 90s, fanny pack banh mi migas
-      </section>
+        <hr />
 
-      <hr />
+        <section id='testimonials'>
+          testimonials about-me Im baby copper mug PBR&B craft beer lo-fi
+          cornhole pork belly vaporware blog hot chicken lyft tattooed. Hammock
+          bruh tote bag, cupping fingerstache flannel affogato enamel pin echo
+          park pabst typewriter gochujang blog green juice. Man braid
+          flexitarian tofu, seitan mixtape crucifix pabst bitters gorpcore
+          skateboard craft beer. Mukbang flexitarian salvia, microdosing copper
+          mug vinyl Brooklyn man braid kombucha mlkshk blog scenester adaptogen
+          snackwave. Ennui cupping hot chicken cliche DSA. Tote bag celiac 90s,
+          fanny pack banh mi migas
+        </section>
 
-      <section id='testimonials'>
-        testimonials about-me Im baby copper mug PBR&B craft beer lo-fi cornhole
-        pork belly vaporware blog hot chicken lyft tattooed. Hammock bruh tote
-        bag, cupping fingerstache flannel affogato enamel pin echo park pabst
-        typewriter gochujang blog green juice. Man braid flexitarian tofu,
-        seitan mixtape crucifix pabst bitters gorpcore skateboard craft beer.
-        Mukbang flexitarian salvia, microdosing copper mug vinyl Brooklyn man
-        braid kombucha mlkshk blog scenester adaptogen snackwave. Ennui cupping
-        hot chicken cliche DSA. Tote bag celiac 90s, fanny pack banh mi migas
-      </section>
+        <hr />
 
-      <hr />
-
-      <section id='home-map'>
-        home-map about-me Im baby copper mug PBR&B craft beer lo-fi cornhole
-        pork belly vaporware blog hot chicken lyft tattooed. Hammock bruh tote
-        bag, cupping fingerstache flannel affogato enamel pin echo park pabst
-        typewriter gochujang blog green juice. Man braid flexitarian tofu,
-        seitan mixtape crucifix pabst bitters gorpcore skateboard craft beer.
-        Mukbang flexitarian salvia, microdosing copper mug vinyl Brooklyn man
-        braid kombucha mlkshk blog scenester adaptogen snackwave. Ennui cupping
-        hot chicken cliche DSA. Tote bag celiac 90s, fanny pack banh mi migas
-      </section>
+        <section id='home-map'>
+          home-map about-me Im baby copper mug PBR&B craft beer lo-fi cornhole
+          pork belly vaporware blog hot chicken lyft tattooed. Hammock bruh tote
+          bag, cupping fingerstache flannel affogato enamel pin echo park pabst
+          typewriter gochujang blog green juice. Man braid flexitarian tofu,
+          seitan mixtape crucifix pabst bitters gorpcore skateboard craft beer.
+          Mukbang flexitarian salvia, microdosing copper mug vinyl Brooklyn man
+          braid kombucha mlkshk blog scenester adaptogen snackwave. Ennui
+          cupping hot chicken cliche DSA. Tote bag celiac 90s, fanny pack banh
+          mi migas
+        </section>
+      </div>
     </HomePageLayout>
   )
 }
