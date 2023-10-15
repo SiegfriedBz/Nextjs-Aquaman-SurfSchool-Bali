a12b8a8a33676eefd2f661620a6f963e3a037764
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-15 22:19:44 +0200

    update pages content

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import PageLayout from '@/components/layouts/PageLayout'
 
 const Gallery = () => {
-  return <div>Gallery</div>
+  return (
+    <PageLayout>
+      <section className={`flex h-full w-full flex-col items-center`}>
+        <h1 className='text-gradient title'>Gallery</h1>
+        <h2 className='text-gradient sub-title'>H2 SUBTITLE</h2>
+        <h3 className='text-gradient'>H3 XXX</h3>
+        <h4 className='text-gradient'>H4 XXX</h4>
+        <p>P XXX</p>
+        <p>P XXX</p>
+        <p>P XXX</p>
+        <p>P XXX</p>
+      </section>
+    </PageLayout>
+  )
 }
 
 export default Gallery
