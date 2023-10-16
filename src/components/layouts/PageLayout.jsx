a58cbed6344b42610e55a53289cfd810a4e64330
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-16 17:27:59 +0200

    add scrollToTop Btn on PageLayout

--- src/components/layouts/PageLayout.jsx
+++ src/components/layouts/PageLayout.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { useRef } from 'react'
 
 const PageLayout = ({ children }) => {
-  return <div className='page-layout'>{children}</div>
+  const ref = useRef(null)
+
+  return (
+    <div ref={ref} className='page-layout relative scroll-mt-[8rem]'>
+      {children}
+      <FontAwesomeIcon
+        icon={faCircleArrowUp}
+        onClick={() => ref.current.scrollIntoView({ behavior: 'smooth' })}
+        className='absolute bottom-[0.35rem] right-0 cursor-pointer text-2xl text-ternary-light transition-all hover:text-ternary'
+      />
+    </div>
+  )
 }
 
 export default PageLayout
