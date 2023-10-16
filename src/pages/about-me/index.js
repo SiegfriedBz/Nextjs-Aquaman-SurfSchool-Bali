52894fc6662d66dd7019afeaa151ae7c4c7704f6
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-16 11:14:24 +0200

    fix AboutMe page ButtonAsGradient btn

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -33,6 +33,7 @@ const AboutMe = ({ aboutMeImg }) => {
         <ButtonAsGradient
           As='Link'
           href={process.env.NEXT_PUBLIC_WHATSAPP_LINK || '/'}
+          target='_blank'
           extraClasses='mx-auto my-4'
         >
           Surf Now
