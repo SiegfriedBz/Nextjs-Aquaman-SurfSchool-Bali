644587d0904019559def7e7cefca95e59926f5ab
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-16 09:00:21 +0200

    move pages layouts css to globals.css

--- src/components/layouts/HomePageLayout.jsx
+++ src/components/layouts/HomePageLayout.jsx
@@ -1,7 +1,5 @@
 const HomePageLayout = ({ children }) => {
-  return (
-    <div className='pb-4 xs:pb-8 sm:pb-12 md:pb-16 lg:pb-24'>{children}</div>
-  )
+  return <div className='home-page-layout'>{children}</div>
 }
 
 export default HomePageLayout
--- src/components/layouts/PageLayout.jsx
+++ src/components/layouts/PageLayout.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 
 const PageLayout = ({ children }) => {
-  return (
-    <div
-      // mt-[5rem] to push the content below the navbar on non-home pages
-      className='mt-[5rem] py-8 xs:py-12 sm:py-16 md:py-24 lg:py-32'
-    >
-      {children}
-    </div>
-  )
+  return <div className='page-layout'>{children}</div>
 }
 
 export default PageLayout
