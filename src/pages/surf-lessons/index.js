5e6e65d997f5852e153f03fdc5776e394f90d560
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-16 09:02:02 +0200

    update SurfLessons page

--- src/pages/surf-lessons/index.js
+++ src/pages/surf-lessons/index.js
@@ -1,17 +1,45 @@
+import { ButtonAsGradient } from '@/components/ButtonAsGradient'
 import PageLayout from '@/components/layouts/PageLayout'
+import Link from 'next/link'
 
 const SurfLessons = () => {
   return (
     <PageLayout>
       <section className={`flex h-full w-full flex-col items-center`}>
         <h1 className='title'>Surf Lessons</h1>
-        <h2 className='sub-title'>H2 SUBTITLE</h2>
-        <h3>H3 XXX</h3>
-        <h4>H4 XXX</h4>
-        <p>P XXX</p>
-        <p>P XXX</p>
-        <p>P XXX</p>
-        <p>P XXX</p>
+        <h2 className='sub-title'>
+          In Canggu &{' '}
+          <Link
+            href='/surf-trips'
+            target='_self'
+            className='text-gradient underline-gradient-link'
+          >
+            Surf Trips
+          </Link>
+        </h2>
+
+        <div className='flex w-full justify-center space-x-8'>
+          <ButtonAsGradient
+            As='Link'
+            href='/surf-lessons'
+            target='_self'
+            variant='btn-as-gradient-amber'
+            padding='px-4 py-2'
+            extraClasses='my-2 w-1/3 text-center'
+          >
+            Private
+          </ButtonAsGradient>
+          <ButtonAsGradient
+            As='Link'
+            href='/surf-lessons/#group'
+            target='_self'
+            variant='btn-as-gradient-amber'
+            padding='px-4 py-2'
+            extraClasses='my-2 w-1/3 text-center'
+          >
+            Group
+          </ButtonAsGradient>
+        </div>
       </section>
     </PageLayout>
   )
