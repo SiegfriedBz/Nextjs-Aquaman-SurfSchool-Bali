db1184a907758234fbaac1608a3aee59e01cc1df
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-17 16:46:36 +0200

    cleanup

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -10,11 +10,6 @@ const RootLayout = ({ children }) => {
     <AppContextProvider>
       <div className={`min-h-screen ${inter.className}`}>
         <Header />
-        {/* <main
-      className={`container px-2 py-4 xs:py-8 sm:py-12 md:py-16 lg:py-24 xl:py-32`}
-      >
-        {children}
-      </main> */}
         <main className={`container px-2 sm:px-0`}>{children}</main>
         <Footer />
       </div>
