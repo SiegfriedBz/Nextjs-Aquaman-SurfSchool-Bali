e5d4bbae13635b6338e00d1d1c23a548ba49665d
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-17 21:20:30 +0200

    fix... meta canonicalUrl

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import { AppContextProvider } from '@/context/appContext'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import { m } from 'framer-motion'
 
 const RootLayout = (props) => {
   const { children, ...customMeta } = props
@@ -10,10 +11,7 @@ const RootLayout = (props) => {
   const router = useRouter()
 
   const meta = {
-    canonicalUrl:
-      router?.asPath != undefined
-        ? `${process.env.NEXT_PUBLIC_URL}${router.asPath}`
-        : `${process.env.NEXT_PUBLIC_URL}`,
+    canonicalUrl: `${process.env.NEXT_PUBLIC_URL}${router.asPath}`,
     ...customMeta,
   }
 
@@ -25,10 +23,7 @@ const RootLayout = (props) => {
           name='keywords'
           content='Aquaman Bali, Surf Aquaman Bali, Aquaman Surf Bali, AquamanBali, Canggu Surf, Canggu Surf School, Canggu Surf Lessons, Bali Surf, Bali Surf Lessons, Bali Surf School, Bali Surf Trips, Bali Surf Photos, Canggu Surf Photos, Bali Surf Videos, Bali Surf Drone'
         ></meta>
-        <meta
-          property='og:url'
-          content={`${process.env.NEXT_PUBLIC_URL}${router.asPath}`}
-        />
+        <meta property='og:url' content={meta.canonicalUrl} />
         <link rel='canonical' href={meta.canonicalUrl} />
         <meta property='og:site_name' content='Aquaman Bali' />
         <meta property='og:image' content={meta.imageUrl} />
