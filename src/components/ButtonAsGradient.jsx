7af87e00cb410a464d33d9e6b498b11d4b2da960
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-18 20:09:00 +0200

    make ButtonAsGradient w-1/2

--- src/components/ButtonAsGradient.jsx
+++ src/components/ButtonAsGradient.jsx
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 
 export const ButtonAsGradient = ({
-  As = 'button',
+  As = 'Link',
   variant = 'btn-as-gradient',
-  padding = 'px-8 py-4',
+  padding = 'px-8 py-2',
   extraClasses = '',
   children,
   ...rest
 }) => {
-  const RenderAs = As === 'button' ? 'button' : Link
+  const RenderAs = As === 'Link' ? Link : 'button'
 
   return (
     <RenderAs
       {...rest}
-      className={`btn-as ${variant} ${padding} ${extraClasses}`}
+      className={`btn-as w-1/2 text-center ${variant} ${padding} ${extraClasses}`}
     >
       {children}
     </RenderAs>
