b7ff01be202378401ae875e6d2eee75fb8abe7c1
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-20 19:42:00 +0200

    refactor NavLinks

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -7,6 +7,79 @@ import {
   WhatsappIconLink,
 } from './Icons'
 
+const NAV_LINKS = (modal) => [
+  {
+    id: 1,
+    href: '/surf-lessons',
+    text: 'Surf Lessons',
+    className: modal ? '' : 'hidden md:inline-block',
+  },
+  {
+    id: 2,
+    href: '/surf-trips',
+    text: 'Surf Trips',
+    className: modal ? '' : 'hidden lg:inline-block',
+  },
+  {
+    id: 3,
+    href: '/photo-video',
+    text: 'Photo & Video',
+    className: modal ? '' : 'hidden xl:inline-block',
+  },
+  {
+    id: 4,
+    href: '/gallery',
+    text: 'Gallery',
+    className: modal ? '' : 'hidden xl:inline-block',
+  },
+  {
+    id: 5,
+    href: '/about-me',
+    text: 'About me',
+    className: modal ? '' : 'hidden 2xl:inline-block',
+  },
+  {
+    id: 6,
+    href: '/#visit-us',
+    text: 'Visit us',
+    className: modal ? '' : 'hidden 2xl:inline-block',
+  },
+  {
+    id: 7,
+    href: '/#testimonials',
+    text: 'Testimonials',
+    className: modal ? '' : 'hidden md:inline-block',
+  },
+]
+
+const SOCIAL_LINKS = (modal) => [
+  {
+    id: 1,
+    className: '',
+    icon: <StreetViewIconLink className={modal ? 'text-[2.75rem]' : ''} />,
+  },
+  {
+    id: 2,
+    className: modal ? '' : 'hidden 2xl:inline-block',
+    icon: <CoffeeIconLink className={modal ? 'text-[2.5rem]' : ''} />,
+  },
+  {
+    id: 3,
+    className: modal ? '' : 'hidden 2xl:inline-block',
+    icon: <TiktokIconLink className={modal ? 'h-12 w-12' : ''} />,
+  },
+  {
+    id: 4,
+    className: modal ? '' : 'hidden xl:inline-block',
+    icon: <IgIconLink className={modal ? 'text-[3rem]' : ''} />,
+  },
+  {
+    id: 5,
+    className: '',
+    icon: <WhatsappIconLink className={modal ? 'text-[3rem]' : ''} />,
+  },
+]
+
 const NavLinks = ({ className = '', modal = false, closeModal }) => {
   return (
     <ul
@@ -14,62 +87,20 @@ const NavLinks = ({ className = '', modal = false, closeModal }) => {
         modal ? 'flex-col' : 'justify-end space-x-4 lg:space-x-6'
       } flex h-full w-full items-center ${className}`}
     >
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden md:inline-block'}
-        modal={modal}
-        href='/surf-lessons'
-      >
-        Surf Lessons
-      </Li>
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden lg:inline-block'}
-        modal={modal}
-        href='/surf-trips'
-      >
-        Surf Trips
-      </Li>
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden xl:inline-block'}
-        modal={modal}
-        href='/photo-video'
-      >
-        Photo & Video
-      </Li>
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden xl:inline-block'}
-        modal={modal}
-        href='/gallery'
-      >
-        Gallery
-      </Li>
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden 2xl:inline-block'}
-        modal={modal}
-        href='/about-me'
-      >
-        About me
-      </Li>
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden 2xl:inline-block'}
-        modal={modal}
-        href='/#visit-us'
-      >
-        Visit us
-      </Li>
-      <Li
-        closeModal={closeModal}
-        className={modal ? '' : 'hidden md:inline-block'}
-        modal={modal}
-        href='/#testimonials'
-      >
-        Testimonials
-      </Li>
+      {/* nav links */}
+      {NAV_LINKS(modal)?.map((link) => {
+        return (
+          <Li
+            key={link.id}
+            href={link.href}
+            className={link.className}
+            modal={modal}
+            closeModal={closeModal}
+          >
+            {link.text}
+          </Li>
+        )
+      })}
 
       {/* social links */}
       <span
@@ -77,44 +108,13 @@ const NavLinks = ({ className = '', modal = false, closeModal }) => {
           modal ? 'mb-4 mt-6 inline-flex w-[19rem] justify-between' : ''
         } inline-flex items-center justify-end space-x-4 lg:space-x-6`}
       >
-        <li
-          onClick={() => {
-            modal && closeModal()
-          }}
-        >
-          <StreetViewIconLink className={modal ? 'text-[2.75rem]' : ''} />
-        </li>
-        <li
-          onClick={() => {
-            modal && closeModal()
-          }}
-          className={modal ? '' : 'hidden 2xl:inline-block'}
-        >
-          <CoffeeIconLink className={modal ? 'text-[2.5rem]' : ''} />
-        </li>
-        <li
-          onClick={() => {
-            modal && closeModal()
-          }}
-          className={modal ? '' : 'hidden 2xl:inline-block'}
-        >
-          <TiktokIconLink className={modal ? 'h-12 w-12' : ''} />
-        </li>
-        <li
-          onClick={() => {
-            modal && closeModal()
-          }}
-          className={modal ? '' : 'hidden xl:inline-block'}
-        >
-          <IgIconLink className={modal ? 'text-[3rem]' : ''} />
-        </li>
-        <li
-          onClick={() => {
-            modal && closeModal()
-          }}
-        >
-          <WhatsappIconLink className={modal ? 'text-[3rem]' : ''} />
-        </li>
+        {SOCIAL_LINKS(modal)?.map((link) => {
+          return (
+            <li key={link.id} className={link.className}>
+              {link.icon}
+            </li>
+          )
+        })}
       </span>
     </ul>
   )
